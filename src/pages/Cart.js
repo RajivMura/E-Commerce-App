ad8743c12c44aa6a35ec974facae297581a2bdde
RajivMura <rajivmura299@gmail.com>
2024-12-09 12:15:35 +0530

    added E-commerce

---
+++ src/pages/Cart.js
@@ -0,0 +1,38 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { useSearchParams } from 'react-router-dom';
+
+export default function Cart() {
+
+  const { products, currency, cartItems } = useContext(ShopContext);
+
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items, 
+            size: item,
+            quantity:cartItems[items][item]
+          })
+        }
+      }
+    }
+    setCartData(tempData);
+    
+    
+  }, [cartItems])
+
+
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
